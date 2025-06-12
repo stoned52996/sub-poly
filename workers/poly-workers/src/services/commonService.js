69fa48e66ef6d6bd0a4eea8bf7da9320c922e88b
stoned52996 <stoned52996@gmail.com>
2025-06-12 11:12:13 +0800

    修复部分订阅地址下载需要请求头 确实导致500报错问题

--- workers/poly-workers/src/services/commonService.js
+++ workers/poly-workers/src/services/commonService.js
@@ -295,9 +295,28 @@ export default {
     // 如果以 &flag=clash结尾
     url = await this.ensureFlagInUrl(url);
     console.log("url: " + url);
+    const parsedUrl = new URL(url);
+    const host = parsedUrl.host; 
     try {
-      const response = await axios.get(url); // 使用await等待响应
-      console.log(response.status);
+      const response = await axios.get(url, {
+        headers: {
+          'Host': host,
+          'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+          'accept-encoding': 'gzip, deflate, br, zstd',
+          'accept-language': 'zh-CN,zh;q=0.9',
+          'priority': 'u=0, i',
+          'sec-ch-ua': '"Google Chrome";v="137", "Chromium";v="137", "Not/A)Brand";v="24"',
+          'sec-ch-ua-mobile': '?0',
+          'sec-ch-ua-platform': '"Windows"',
+          'sec-fetch-dest': 'document',
+          'sec-fetch-mode': 'navigate',
+          'sec-fetch-site': 'none',
+          'sec-fetch-user': '?1',
+          'upgrade-insecure-requests': '1',
+          'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36'
+        }
+      });
+      console.log("下载配置文件" + response.status);
       if (response.status === 200) {
         const jsonData = yaml.load(response.data);
         const subscriptionInfo = response.headers['subscription-userinfo'];
